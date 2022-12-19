7900f662c7b7e83486db1c05389970a33a12f6ee
User <ismail.karim@inbox.ru>
2022-12-19 21:07:22 +0600

    add program

--- handlers/basic_handlers/lowprice.py
+++ handlers/basic_handlers/lowprice.py
@@ -1,4 +1,5 @@
-from typing import Dict, List
+from typing import Dict
+from typing import List
 from telebot.types import Message
 from loader import bot
 from config_data.contact_information import User
