1eadbca800138ded7e41a5dfb5824ffb0b2f6fa0
Карим Исмаил <ismail.karim01@inbox.ru>
2022-12-20 21:07:30 +0000

    Delete currency.py

--- keyboards/inline/currency.py
+++
@@ -1,9 +0,0 @@
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-
-def get_inline_buttons_currency():
-    keyboard = InlineKeyboardMarkup()
-    for currency in ['RUB', 'USD', 'EUR']:
-        key = InlineKeyboardButton(text=currency, callback_data=currency)
-        keyboard.add(key)
-    return keyboard
