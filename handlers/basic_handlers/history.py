ea463b7cb4b76ce045e0fd5dd0a3aff3241a0cae
Карим Исмаил <ismail.karim01@inbox.ru>
2022-12-20 21:06:15 +0000

    Delete history.py

--- handlers/basic_handlers/history.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import Message
-from loader import bot
-from database.user_data import get_history
-
-
-@bot.message_handler(commands=['history'])
-def command_history(message: Message) -> None:
-    bot.send_message(message.chat.id, get_history(message.from_user.id))
